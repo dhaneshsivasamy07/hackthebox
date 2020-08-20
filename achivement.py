6c9a70b499fc4a6e7362479fe90a76a9c027c2d9
Dhanesh Sivasamy <55705969+cyberwr3nch@users.noreply.github.com>
2020-08-20 22:06:38 +0530

    Update achivement.py
    Added API input functionalities and Output Messages
    
    Former-commit-id: 518c72d2a40246285679fbbb18e670b247489362
    Former-commit-id: dab289bb795ab6f91b62dbc403d0c15cdaf0aeba

--- achivement.py
+++ achivement.py
@@ -2,35 +2,90 @@
 ##############################################################################################
  provide your htb user id, machine Id and machine name
  user id : https://www.hackthebox.eu/home/users/profile/220867, 220867 9s the profile id
- machine id: https://www.hackthebox.eu/home/machines/profile/233, 223 is the machine id
+ api key can be found in `hackthebox.eu/home/settings`
+ enter the machine name and get snapped =)
 
 required modules
 1. requests - pip3 install requests
 2. imgkit - pip3 install imgkit
-
+3. coloroma - pip install colorama
 
 ###############################################################################################
 '''
 
 import requests
+import sys
+from colorama import Fore, Back, Style
+import json
 import imgkit
 
-# variables
-url_ = "https://www.hackthebox.eu/achievement/machine/"
-header = {'User-agent': 'Mozilla/5.0'}
-
-#inputs
-userId = input('Enter HackTheBox User ID: ')
-macId = input('Enter machine ID: ')
-macNam = input("Enter machine name: ")
-
-#processing
-dn = url_ + userId + '/' + macId 
-r = requests.get (url = dn, headers = header)
-if "Invalid" in r.text:
-	print("The machine ID seems to be invalid / you havent finished it yet")
-else:
-	print('Processing the snapshot...')
-	imgkit.from_url( dn , macNam +'.jpg')
+BOLD = '\033[1m'
+UNBOLD = "\033[0;0m"
 
 
+def gen():
+	#variables
+	URL = 'https://www.hackthebox.eu/api/machines/get/all?api_token='
+	api_ = input("Enter your API Key: ")
+	URL += api_
+
+
+	ach_ = 'https://www.hackthebox.eu/achievement/machine/'
+	userId = input("Enter your htb id: ")
+	macNam = ''
+
+	HEADERS = { 'User-agent': 'Mozilla 5.0'}
+	macId = ''
+# process
+	r = requests.get(url = URL, headers = HEADERS)
+	if r.status_code == 200:
+		machines = r.json()
+
+		name = input("Machine name: ")
+		macNam = name
+		for machine in machines:
+			if machine['name'].lower() == name.lower():
+				macId = str(machine['id'])
+
+
+	print("==================================================================")
+	print("Capturing the " + macNam + " machine")
+	print("==================================================================")
+
+
+	dn = ach_ + userId + '/' + macId
+	red = requests.get(url = dn, headers = HEADERS)
+	#print(dn)
+
+	if "Invalid" in red.text:
+		print("Looks like, you Haven't completed the "+ macNam + "machine yet (>_<)")
+	else:
+		print("Congratulations on completing the box =D")
+		print('Processing the snapshot...')
+		imgkit.from_url( dn , macNam +'.jpg')
+
+
+
+def logo():
+    print(Fore.LIGHTGREEN_EX + """ 
+
+    	                                                               .oooo.                         oooo        
+                                                                    .dP""Y88b                        `888        
+ .oooo.o ooo. .oo.    .oooo.   oo.ooooo.  oooo oooo    ooo oooo d8b       ]8P' ooo. .oo.    .ooooo.   888 .oo.   
+d88(  "8 `888P"Y88b  `P  )88b   888' `88b  `88. `88.  .8'  `888""8P     <88b.  `888P"Y88b  d88' `"Y8  888P"Y88b  
+`"Y88b.   888   888   .oP"888   888   888   `88..]88..8'    888          `88b.  888   888  888        888   888  
+o.  )88b  888   888  d8(  888   888   888    `888'`888'     888     o.   .88P   888   888  888   .o8  888   888  
+8""888P' o888o o888o `Y888""8o  888bod8P'     `8'  `8'     d888b    `8bd88P'   o888o o888o `Y8bod8P' o888o o888o 
+                                888                                                                              
+                               o888o          
+
+                               """ + Fore.RESET)
+
+
+if __name__ == "__main__":
+    logo()
+    print("Simple SnapShot tool by wr3nch")
+    print(BOLD + 'Coded by a n00b: '+ Fore.BLUE + 'cyberwr3ch' + Fore.LIGHTBLACK_EX + '\nMember of TCSC' + Fore.RESET)
+    print('With the help of my bros, AdithyanAK and Gokul' + UNBOLD)
+    gen()
+    print("\n Happy Hacking")
